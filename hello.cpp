5ccb570613871a312a0d8fd48fda8bacf8ecaef1
Evan Morrison <epmorrison@mavs.coloradomea.edu>
2024-01-25 10:03:58 -0700

    Updated Version

--- hello.cpp
+++ hello.cpp
@@ -13,6 +13,6 @@ int main()
 {
     //This will print out hello world to the console
     cout << "Hello World" << endl;
-    cout << "This is a second wassup my bruddas" << endl;
+    cout << "This is a second wassup to my bruddas" << endl;
     return 0; //This exits the program with value 0
 }

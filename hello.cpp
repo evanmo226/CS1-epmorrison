dc8d6aa7650b2f5a1b70693f98b2c24dab855cbe
Evan Morrison <epmorrison@mavs.coloradomea.edu>
2024-01-25 09:50:27 -0700

    Crreated Hello World!

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+/*
+Name: Evan Morrison
+
+First Hello World Program
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //This will print out hello world to the console
+    cout << "Hello World" << endl;
+    cout << "This is a second hello my bruddas" << endl;
+    return 0; //This exits the program with value 0
+}

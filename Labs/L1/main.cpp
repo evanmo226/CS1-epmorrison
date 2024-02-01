e658c000d04728135be1618e1bbf09becf616107
Evan Morrison <epmorrison@mavs.coloradomea.edu>
2024-02-01 14:02:53 -0700

    Fixed Main

--- Labs/L1/main.cpp
+++ Labs/L1/main.cpp
@@ -12,20 +12,18 @@
     2. Write a series of cout statements to print the data/values
 */
 
-#include <iostream> //library for input and output
-#include <string> //library for string data
+#include <iostream>
+#include <string>
  
-using namespace std; //resolve cout, cin, and endl names
+using namespace std;
  
-//main entry point of the program
 int main()
 {
-    string name;//FIXME3: declare a variable to store name
-    cout << "Enter your name here: ";
-    cin >> name; //FIXME4: prompt user to enter their name and store the value in variable
-    
-    cout << "It is great to meet you " << name << "!" << endl;//FIXME5: greet the name using the variable as the following output
-    //must output: Nice meeting you, <name>!
+    string name;
+    cout << "Enter your name here: ";  //FIXED
+    cin >> name;    //FIXED
+
+    cout << "It is great to meet you " << name << "!" << endl;  //FIXED
 
     cout << "Hope you like my Tom & Jerry ASCII art...\n\n";
 
@@ -33,23 +31,20 @@ int main()
     cout << line1;
 
     string line2 = "   / @ @ \\ \t*           ASCII Lab     \t*    (='.'=)\n";
-    cout << line2;
-    //FIXME6: use variable to print the second line line of the graphic
-    cout << "  (>  0  <) \t*      By: Evan Morrison  \t*    (\")_(\")\n";
+    cout << line2;  //FIXED
 
-    //FIXME7: print the third line of the graphics
-    
+    cout << "  (>  0  <) \t*      By: Evan Morrison  \t*    (\")_(\")\n";   //FIXED
+
     string line4 = "    >>x<<   \t*           CSCI 130     \t*";
-    cout << line4 << endl;
-    //FIXME8: use variable to print the fourth line
+    cout << line4 << endl; //FIXED
+
     string line5 = "   /  O  \\   \t*********************************\n";
-    cout << line5;
-    //FIXME9: use variable to print the fifth line
-    //Note: You can add more lines or print more ASCII arts of your choice if you'd like...
+    cout << line5;  //FIXED
+
 
     cout << "\nGood bye... hit enter to exit the program: " << '\n';
-    
+
     //FIXME10: make the console wait for user input
 
-    return 0; //exit program by returning 0 status to the system
+    return 0;
 }

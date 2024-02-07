2ffe5ccff89c8eb227d4319b3db302dff36e6c9c
Evan Morrison <epmorrison@mavs.coloradomea.edu>
2024-02-07 09:42:24 -0700

    Testing

--- variables.cpp
+++ variables.cpp
@@ -6,26 +6,61 @@ All about the variables
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <cassert>
+#include <cstdlib>
+#include <cstdio>
 
 using namespace std;
 
 
 int main()
 {
-    int origSeconds;
-    int numSeconds;
-    int numMinutes;
-    int numHours;
 
-    cout << "Enter number of seconds: ";
-    cin >> origSeconds;
-    numSeconds = origSeconds;
+    string phrase;
 
-    numHours = numSeconds/3600;
-    numMinutes = (numSeconds/60) - (numHours*60);
-    numSeconds = numSeconds%60;
+    phrase = "this is a sentence";
+    
+    // float n1, n2;
+    // float sum;
+    // cout << "Enter number 1: ";
+    // cin>> n1;
 
-    cout << "The hours:minutes:seconds given " << origSeconds << " seconds is: \n"
-        << numHours << ":" << numMinutes << ":" << numSeconds << endl;
+    // cout << "Enter number 2:";
+    // cin >> n2;
+
+    // sum = n1+ n2;
+    // assert(sum == 57);
+
+    // cout << n1 << " + " << n2 << " = " << sum << endl;
+    // printf("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int origSeconds;
+    // int numSeconds;
+    // int numMinutes;
+    // int numHours;
+
+    // cout << "Enter number of seconds: ";
+    // cin >> origSeconds;
+    // numSeconds = origSeconds;
+
+    // numHours = numSeconds/3600;
+    // numMinutes = (numSeconds/60) - (numHours*60);
+    // numSeconds = numSeconds%60;
+
+    // cout << "The hours:minutes:seconds given " << origSeconds << " seconds is: \n"
+    //     << numHours << ":" << numMinutes << ":" << numSeconds << endl;
     return 0;
 }

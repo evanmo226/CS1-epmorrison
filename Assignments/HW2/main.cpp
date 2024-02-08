a8276d38bf5a9441de64aa745f73bad5e97c64ec
Evan Morrison <epmorrison@mavs.coloradomea.edu>
2024-02-08 09:02:38 -0700

    Changed

--- Assignments/HW2/main.cpp
+++ Assignments/HW2/main.cpp
@@ -3,5 +3,18 @@
 
     By: Evan Morrison
     Date: Feb 7 2024
-    
+
+    Program find perimeter and area of a triangle given inputs of three side lengthes.
+
 */
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+
+}

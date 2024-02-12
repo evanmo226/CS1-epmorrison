1e2be8bb6eff04d1caa55caa5508b270ab48b081
Evan Morrison <epmorrison@mavs.coloradomea.edu>
2024-02-12 16:59:20 -0700

    Updated HW2 code

--- Assignments/HW2/main.cpp
+++ Assignments/HW2/main.cpp
@@ -4,7 +4,8 @@
     By: Evan Morrison
     Date: Feb 7 2024
 
-    Program find perimeter and area of a triangle given inputs of three side lengthes.
+    Program find perimeter and area of a 
+    triangle given inputs of three side lengths.
 
 */
 
@@ -15,6 +16,48 @@
 using namespace std;
 
 int main() {
+    //Declaring variables for the program
+    int side1, side2, side3;
+    int perim;
+    double area;
 
+    // Prompts user to enter side lengths
+    cout << "Hello, please enter three side lengths of the triangle." << endl; 
 
+    //User enters side names into the three variables
+    cout << "Side 1: ";
+    cin >> side1;
+    cout << "Side 2: ";
+    cin >> side2;
+    cout << "Side 3: ";
+    cin >> side3;
+
+    //Making sure the side lengths actually form a triangle
+    if(side1+side2 < side3){
+        cout << "The given numbers do not form a triangle. Please try again." << endl;
+        return 0;
+    }
+    if(side3+side2 < side1){
+        cout << "The given numbers do not form a triangle. Please try again." << endl;
+        return 0;
+    }
+    if(side1+side3 < side2){
+        cout << "The given numbers do not form a triangle. Please try again." << endl;
+        return 0;
+    }
+
+    //If the code got to this point, a triangle is possible
+    cout << "\nThe given triangle is possible." << endl;
+
+    //Calculating the perimeter of the triangle
+    perim = side1 + side2 + side3;
+
+    //Calculating the area of the triangle using Heron's formula
+    double s = (perim)/2; //using perim to solve for the s value of the formula
+    area = sqrt(s*(s-side1)*(s-side2)*(s-side3));
+
+    cout << "The perimeter of the triangle is: " << perim << endl;
+    cout << "The area of the triangle is: " << area << endl;
+
+    return 0;
 }
---
+++ functions.cpp
@@ -0,0 +1,44 @@
+/*
+Name: Evan Morrison
+
+Functions
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void sayHello()
+{
+    cout << "Hello World" << endl;
+}
+
+int addToNumber()
+{   
+    int someNumber;
+    cout << "Please enter a number: ";
+    cin >> someNumber;
+    someNumber += 10;
+    return someNumber;
+}
+
+char giveMeAChar()
+{
+    return 'e';
+}
+int addNums(int one, int two)
+{
+    return one + two;
+}
+int main()
+{
+    int answer;
+    int num1, num2;
+    cout << "Enter two numbers separated by a space: ";
+    cin >> num1 >> num2;
+
+    answer = addNums(num1, num2);
+    cout << num1 << " + " << num2 << " = " << answer << endl;
+
+    return 0;
+}
